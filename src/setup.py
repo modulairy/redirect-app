f253f2607ba345de14a4628a19690c38308a5b89
Fatih Mehmet ARSLAN <fatiharslan@modulairy.com>
2023-10-26 21:35:49 +0300

    wf

--- src/setup.py
+++ src/setup.py
@@ -10,11 +10,6 @@ setup(
         'Flask',
         'gunicorn'
     ],
-    entry_points={
-        'console_scripts': [
-            'modulairy-redirect-app = gunicorn -w 4 -b 0.0.0.0:8000 main:app'
-        ]
-    },
     author='Fatih Mehmet ARSLAN',
     author_email='contact@fmarslan.com',
     description='Modulairy Redirect App is a flexible Flask-based redirection application that dynamically redirects incoming HTTP requests based on environment variables prefixed with TARGET_.',
